d445a2c9637ca993da15052b47b1a9e86a4f00f1
Safat Alnur <55736684+safatalnur@users.noreply.github.com>
2020-02-12 13:05:12 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Games from './components/Games.js';
+
+function App () {
+  return (
+      <Games/>
+    )
+  };
+
+export default App;
---
+++ components/Games.js
@@ -0,0 +1,93 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Platform,
+  Button,
+} from 'react-native';
+
+
+function Games() {
+
+    // const [numberCount, setNumberCount] = useState ([]);
+    // const [randomNumber, setRandomNumber] = useState([]);
+
+    const [randomNumbersArray, setRandomNumbersArray] = useState([]);
+
+    // const randomNumbers = [...Array(6)].map(()=>Math.floor(Math.random()*10));
+    // .from({lenght: 6}),
+    // ()=> 1 + Math.floor(10 * Math.random());
+    
+    // const onTarget = () => randomNumbers.reduce(function(a, b) { return a + b; }, 0);
+    // const onTarget = sum(randomNumbers);
+
+    // const onTarget = () => randomNumbers.reduce((acc, curr) => acc + curr,0)
+    // const onTarget = () => [randomNumbers].(Math.sum(randomNumbers))
+
+    function handlePress() {
+        const newRandomNumbers = [...Array(6)].map(()=>Math.floor(Math.random()*10));
+        setRandomNumbersArray(newRandomNumbers);
+        // setTarget(sum.randomNumbers);
+        // return setTarget;
+        // setTarget(10 + Math.floor(40 * Math.random()));
+    };
+
+    function reduceArray(){
+        return randomNumbersArray.reduce((acc, curr) => acc + curr, 0)
+    }
+
+    // const target = 10 + Math.floor(40 * Math.random());
+
+
+    
+  return (
+    <ScrollView style={styles.MainContainer}>
+        <View style={styles.container}>
+            <Text style={styles.target}>The number is : {reduceArray()}</Text>
+            <Button title="Press Me" onPress={() => {
+                handlePress()
+            }}></Button>
+        </View>
+
+        <View >
+            <Text style={styles.randomContainer}>{randomNumbersArray}</Text>
+        </View>
+        
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+    MainContainer: {
+      backgroundColor: '#f5f5f5',
+    },
+
+    target: {
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center', 
+        backgroundColor: '#aaa', 
+        fontSize: 40,
+        marginHorizontal: 50,
+        ...Platform.select({
+            ios: {
+                marginTop: '15%',
+            },
+            android: {
+                marginTop: '15%',
+            },
+        }),
+    },
+
+    randomContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center'
+    }
+});
+
+export default Games;
